c67983d3aafa0bed8dc1e136f57b9af91305114d
Vidit Tiwari <vidittiwari95@gmail.com>
2014-12-10 00:46:52 +0530

    function-edit

--- All-subsets-of-a-set.cpp
+++ All-subsets-of-a-set.cpp
@@ -3,15 +3,31 @@
 using namespace std;
 
 void printPowerset (int n){
-    int stack[n+1],k,;
+    int stack[n+1],k;
 
     stack[0]=0; /* 0 is not considered as part of the set */
     k = 0;
 
+    while(1){
+        if (stack[k]<n){
+            stack[k+1] = stack[k] + 1;
+            k++;
+        }
+
+        else{
+            stack[k-1]++;
+            k--;
+        }
+
+        if (k==0)
+            break;
+        ctr++;
+        printSet(stack,k);
+    }
 
 
+    return;
 }
-
 int main(){
     int n;
     cout<<"Enter the number\n";
