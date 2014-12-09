b71b49b34b76f6d07b98ba67ca70a99648ae7573
Vidit Tiwari <vidittiwari95@gmail.com>
2014-12-09 20:16:16 +0530

    Subset-algo

---
+++ All-subsets-of-a-set.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+void printPowerset (int n){
+    int stack[n+1],k,;
+
+    stack[0]=0; /* 0 is not considered as part of the set */
+    k = 0;
+
+
+
+}
+
+int main(){
+    int n;
+    cout<<"Enter the number\n";
+    cin>>n;
+    printPowerset(n);
+
+    return 0;
+}
